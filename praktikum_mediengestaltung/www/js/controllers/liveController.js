f10215a9ee56a2a205d054271273b66243eccf2e
Maximilian Körner <maximilian.koerner@gmx.com>
2016-08-26 21:55:24 +0200

    switched from css to sass for styling; implemented dummy image slider

--- praktikum_mediengestaltung/www/js/controllers/liveController.js
+++ praktikum_mediengestaltung/www/js/controllers/liveController.js
@@ -1,6 +1,26 @@
 angular.module('prakMed').controller('liveController', function ($scope) {
 
-  console.log("whatever");
+
+  $scope.options = {
+    loop: false,
+    effect: 'fade',
+    speed: 500
+  }
+
+  $scope.$on("$ionicSlides.sliderInitialized", function(event, data){
+    // data.slider is the instance of Swiper
+    $scope.slider = data.slider;
+  });
+
+  $scope.$on("$ionicSlides.slideChangeStart", function(event, data){
+    console.log('Slide change is beginning');
+  });
+
+  $scope.$on("$ionicSlides.slideChangeEnd", function(event, data){
+    // note: the indexes are 0-based
+    $scope.activeIndex = data.activeIndex;
+    $scope.previousIndex = data.previousIndex;
+  });
 
 
 });
