0cd6803dd433483679752ccf81007edee86fcad0
Maximilian Körner <maximilian.koerner@gmx.com>
2016-09-05 11:25:32 +0200

    added thumbnails to hotspot states and removed git merge leftovers

--- praktikum_mediengestaltung/www/js/controllers/galleryController.js
+++ praktikum_mediengestaltung/www/js/controllers/galleryController.js
@@ -4,76 +4,97 @@ angular.module('water').controller('galleryController', function ($scope) {
   //list of all available hotspots
   $scope.hotspots = {
     "1":{
-      name: "teststate 01",
-      state: "app.01"
+      name: "Wasser",
+      state: "app.01",
+      thumbnail: "../img/high_res/Hotspot_01.jpg"
+      //TODO: final image!!
+
     },
     "2":{
       name: "Eyjafjallajoekull Gletscher - Island",
-      state: "app.02"
+      state: "app.02",
+      thumbnail: "../img/high_res/Hotspot_02.jpg"
     },
     "3":{
       name: "Thjorsa - Island",
-      state: "app.03"
+      state: "app.03",
+      thumbnail: "../img/high_res/Hotspot_03.jpg"
     },
     "4":{
       name: "Roebuck Bay - Australien",
-      state: "app.04"
+      state: "app.04",
+      thumbnail: "../img/high_res/Hotspot_04.jpg"
     },
     "5":{
       name: "Perito Moreno - Argentinien",
-      state: "app.05"
+      state: "app.05",
+      thumbnail: "../img/high_res/Hotspot_05.jpg"
     },
     "6":{
       name: "Champagne Pool - Neuseeland",
-      state: "app.06"
+      state: "app.06",
+      thumbnail: "../img/high_res/Hotspot_06.jpg"
     },
     "7":{
       name: "Adriatief - Österreich",
-      state: "app.07"
+      state: "app.07",
+      thumbnail: "../img/high_res/Hotspot_07.jpg"
     },
     "8":{
       name: "Costa Vicentina - Portugal",
-      state: "app.08"
+      state: "app.08",
+      thumbnail: "../img/high_res/Hotspot_08.jpg"
     },
     "9":{
       name: "Gullfoss - Island",
-      state: "app.09"
+      state: "app.09",
+      thumbnail: "../img/high_res/Hotspot_09.jpg"
     },
     "10":{
       name: "Isluga - Chile",
-      state: "app.10"
+      state: "app.10",
+      thumbnail: "../img/high_res/Hotspot_10.jpg"
     },
     "11":{
       name: "Viedma Getscher - Argentinien",
-      state: "app.11"
+      state: "app.11",
+      thumbnail: "../img/high_res/Hotspot_11.jpg"
     },
     "12":{
       name: "Krka - Kroatien",
-      state: "app.12"
+      state: "app.12",
+      thumbnail: "../img/high_res/Hotspot_12.jpg"
     },
     "13":{
       name: "Lake Natron - Kenya",
-      state: "app.13"
+      state: "app.13",
+      thumbnail: "../img/high_res/Hotspot_13.jpg"
     },
     "14":{
       name: "Perito Moreno - Argentinien",
-      state: "app.14"
+      state: "app.14",
+      thumbnail: "../img/high_res/Hotspot_01.jpg"
+      //TODO: final image!!
     },
     "15":{
       name: "Triftgletscher - Schweiz",
-      state: "app.15"
+      state: "app.15",
+      thumbnail: "../img/high_res/Hotspot_15.jpg"
     },
     "16":{
       name: "Lenadelta - Russland",
-      state: "app.16"
+      state: "app.16",
+      thumbnail: "../img/high_res/Hotspot_16.jpg"
     },
     "17":{
       name: "Meereis - Grönland",
-      state: "app.17"
+      state: "app.17",
+      thumbnail: "../img/high_res/Hotspot_17.jpg"
     },
     "18":{
       name: "Petersen Fjord - Grönland",
-      state: "app.18"
+      state: "app.18",
+      thumbnail: "../img/high_res/Hotspot_18.jpg"
     }
   };
 
