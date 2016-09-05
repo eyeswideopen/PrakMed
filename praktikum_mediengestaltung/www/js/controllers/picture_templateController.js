16d762fca06eb2eeef96c25e29ba7e27e4967cfe
Maximilian Körner <maximilian.koerner@gmx.com>
2016-09-05 09:00:46 +0200

    changed quiz functionality to allow multiple instances in one DOM

--- praktikum_mediengestaltung/www/js/controllers/picture_templateController.js
+++ praktikum_mediengestaltung/www/js/controllers/picture_templateController.js
@@ -1,4 +1,4 @@
-angular.module('water').controller('picture_template', function ($scope, waterAudio, MediaManager, $timeout) {
+angular.module('water').controller('picture_template', function ($scope, waterAudio, MediaManager, $timeout, $cordovaBeacon) {
 
 
 
@@ -51,53 +51,46 @@ angular.module('water').controller('picture_template', function ($scope, waterAu
   //
   //QUIZ ELEMENT
   //
-  $scope.answeredRight = false;
-  $scope.answeredWrong = false;
-  var quizInteractionActive = true;
 
   $scope.right = function ($event) {
-    if (quizInteractionActive) {
-      quizInteractionActive = false;
-      $scope.answeredRight = true;
-      //jquery styling of buttons
-      jQuery(".wrong_answer").each(function () {
+    if (!jQuery($event.target).hasClass("button_right_answer")) {
+      var parent = jQuery($event.target).parent();
+      parent.find(".answered_right").show();
+      parent.find(".wrong_answer").each(function () {
         jQuery(this).addClass("disabled");
       });
-      jQuery(".right_answer").addClass("button_right_answer");
+      parent.find(".right_answer").addClass("button_right_answer");
 
     }
   };
 
   $scope.wrong = function ($event) {
-    if (quizInteractionActive) {
-      quizInteractionActive = false;
-      $scope.answeredWrong = true;
-      //jquery styling of buttons
-
-      jQuery(".quiz_button").each(function () {
-
+    if (!jQuery($event.target).hasClass("button_wrong_answer")) {
+      var parent = jQuery($event.target).parent();
+      parent.find(".answered_wrong").show();
+      parent.find(".quiz_button").each(function () {
         if (jQuery(this).is(jQuery($event.target))) {
           jQuery($event.target).addClass("button_wrong_answer");
         } else {
+          console.log("added ")
+
           jQuery(this).addClass("disabled");
         }
       });
+
+      //reset display after 5 sec
+      $timeout(reset.bind(null,parent), 5000);
     }
 
-    //reset display after 5 sec
-   $timeout(reset, 5000);
   };
 
-  var reset = function () {
-    $scope.$apply(function () {
-      $scope.answeredRight = false;
-      $scope.answeredWrong = false;
-    });
-    quizInteractionActive = true;
-    jQuery(".quiz_button").each(function () {
+  //resets view after wrong answer and 5 sec wait
+  var reset = function (quiz) {
+    jQuery(quiz).find(".quiz_button").each(function () {
       jQuery(this).removeClass("button_wrong_answer");
       jQuery(this).removeClass("disabled");
     });
+    jQuery(quiz).find(".answered_wrong").hide();
   }
 })
 ;
